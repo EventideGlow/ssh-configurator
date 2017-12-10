09310105e9ce6b7b7b47e6fcd27581cd87372817
Andres Rama <and-yrama@live.com>
2017-12-10 02:27:57 -0500

    Initial changes

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,9 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_vars_loaded(host):
+    vars = host.ansible.get_variables()
+    print(vars)
+    assert len(vars['users']) == 3
